a6ad0afc3ff9c18e62eb613b50e26b638346ec3c
luyanfei <luyanfei78@163.com>
2025-04-23 16:00:20 +0800

    add musicians list view.

--- bands/urls.py
+++ bands/urls.py
@@ -3,4 +3,5 @@ from bands import views
 
 urlpatterns = [
     path('musician/<int:musician_id>/', views.musician, name='musician'),
+    path('musicians/', views.musicians, name='musicians'),
 ]
--- bands/views.py
+++ bands/views.py
@@ -5,4 +5,6 @@ def musician(request, musician_id):
     musician = get_object_or_404(Musician, id=musician_id)
     return render(request, 'musician.xhtml', {'musician': musician})
 
-
+def musicians(request):
+    musicians = Musician.objects.all().order_by('last_name')
+    return render(request, 'musicians.xhtml', {'musicians': musicians})
