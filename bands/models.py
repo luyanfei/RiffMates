a1cb0fe0de9261cb516cf6278d177494930a5d68
luyanfei <luyanfei78@163.com>
2025-04-23 15:35:37 +0800

    add Musician model.

--- bands/models.py
+++ bands/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth = models.DateField()
+    def __str__(self):
+        return f'Musician {self.id}: {self.first_name} {self.last_name}'
