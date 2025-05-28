4018c6dbc3013a4ea383ea7f9f3705e1b1ac83bb
luyanfei <luyanfei78@163.com>
2025-05-28 14:45:53 +0800

    comment form.

---
+++ content/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from content.forms import CommentForm
+
+def comment(request):
+    if request.method == 'GET':
+        form = CommentForm()
+    else:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            comment = form.cleaned_data['comment']
+            message = f'Name: {name}\nComment: {comment}'
+            send_mail('Received comment', message, 'from@example.com', ['to@example.com'], fail_silently=False)
+            return redirect('comment_accepted')
+    data = {'form': form}
+    return render(request, 'comment.xhtml', data)
+
+def comment_accepted(request):
+    data = {
+        'content': 'Thank you for your comment. We will review it shortly.'
+    }
+    return render(request, 'general.xhtml', data)
+
